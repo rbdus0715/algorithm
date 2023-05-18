55ec61b69c09dcb15a69b0fb6772beeadd32911e
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-05-18 19:45:20 +0900

    Update and rename 006_travelingSalesManProblem_#.cpp to 006_travelingSalesManProblem.cpp

--- study/006_travelingSalesManProblem.cpp
+++ study/006_travelingSalesManProblem.cpp
@@ -2,22 +2,22 @@
 #include <vector>
 using namespace std;
 
-int N, visited[8];
+int N;
 double map[8][8];
 
-double solve(int start, int toGo){
+double solve(int start, int toGo, bool visited[8]){
     if(toGo==0) return map[start][0];
-    cout << start << " ";
-    double sum = 2e9;
+    double result = 2e9;
     
     for(int i=0; i<N; i++){
         if(!visited[i]){ 
             visited[i] = true;
-            sum = min(sum, solve(i, toGo-1)+map[start][i]);
+            result = min(result, solve(i, toGo-1, visited)+map[start][i]);
+            visited[i] = false;
         }
     }
     
-    return sum;
+    return result;
 }
 
 int main(){
@@ -27,6 +27,7 @@ int main(){
             cin >> map[i][j]; 
         }
     }
+    bool visited[8] = {0,};
     visited[0] = true;
-    cout << solve(0, N);
+    cout << solve(0, N-1, visited);
 }
