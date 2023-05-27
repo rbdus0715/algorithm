6c309f2888e465a5fadb7f9bfeae1814da93c357
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-05-27 09:15:08 +0900

    Update 6603.cpp

--- boj/recursion/6603.cpp
+++ boj/recursion/6603.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int N, a[49];
 
+// 내 풀이
 void solve(int cur, int n, int result[6]) {
     if(n==0) {
         result[5] = a[cur];
@@ -16,6 +17,34 @@ void solve(int cur, int n, int result[6]) {
             solve(i, n-1, result);
         }
     }
+}
+
+// 다른 풀이 (1)
+/*
+나랑 다른점:
+(1) dfs로 풀음
+(2) 함수에 인자로 배열을 쓰지 않음
+모든 케이스를 전부 살핀다.
+첫 시작이 입력받은 배열의 맨 마지막 원소부터 시작할 수도 있는데 그 케이스도 처리하긴 하지만
+결국에는 종료조건이나 재귀조건 둘다 만족시키지 못하므로 그 케이스는 실현되지 않음
+*/
+void dfs(int start, int depth) {
+	
+	//종료조건
+	if (depth == 6) {
+		for (int i = 0; i < 6; i++) {
+			cout << ans[i] << " ";
+		}
+		cout << endl;
+		return;
+	}
+
+	//재귀: 재귀가 return 되면 다시 다음문자에 대해 dfs
+	for (int i = start; i < num; i++) {
+		ans[depth] = lotto[i];
+		dfs(i + 1, depth+1);
+	}
+
 }
 
 int main() {
