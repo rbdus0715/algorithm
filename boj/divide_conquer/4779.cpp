baf79ede266984acca8e3b0bd49018b87c8399e7
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-06-02 22:10:42 +0900

    Update 4779.cpp

--- boj/divide_conquer/4779.cpp
+++ boj/divide_conquer/4779.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int N;
 
-void solve(int i, int size) {
+void solve(int size) {
     
+    // 기저 사례
+    if(size==3) cout << "- -";
+    else {
+        int nextSize = size/3;
+        
+        // 앞
+        solve(nextSize);
+        
+        // 가운데
+        for(int i=0; i<nextSize; i++) cout << " ";
+        
+        // 뒤
+        solve(nextSize);
+    }
 }
 
 int main() {
     cin >> N;
     int size = pow(3, N);
-    solve(i, size);
+    solve(size);
 }
 
 /*
