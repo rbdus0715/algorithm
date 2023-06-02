f7e3e47d8f029bd51f122ad74a795dd22bcf5cee
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-06-02 20:20:02 +0900

    Create 4779.cpp

---
+++ boj/divide_conquer/4779.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+void solve(int i, int size) {
+    
+}
+
+int main() {
+    cin >> N;
+    int size = pow(3, N);
+    solve(i, size);
+}
+
+/*
+---------------------------
+---------         ---------
+---   ---         ---   ---
+- -   - -         - -   - -
+*/
