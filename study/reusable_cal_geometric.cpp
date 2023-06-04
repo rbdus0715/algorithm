a6622409beea806b1380ee87686900ef75bd8b50
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-06-04 23:45:48 +0900

    Update reusable_cal_geometric.cpp

--- study/reusable_cal_geometric.cpp
+++ study/reusable_cal_geometric.cpp
@@ -2,12 +2,12 @@
 #include <cmath>
 using namespace std;
 
-const double PI = 2.0*acos(0.0); 
+const double PI = 2.0*acos(0.0);
 
 // 2차원 벡터
 struct vector2 {
     double x, y;
-    
+   
     // 생성자 함수
     explicit vector2(double x_ = 0, double y_ = 0) : x(x_), y(y_) {}
 
@@ -15,10 +15,10 @@ struct vector2 {
     bool operator == (const vector2& rhs) const {
         return x == rhs.x && y == rhs.y;
     }
-    bool operator < (const vector2& rhs) const { 
+    bool operator < (const vector2& rhs) const {
         return x != rhs.x ? x < rhs.x : y < rhs.y;
     } // 끝점의 x 좌표가 작은 벡터일수록, x좌표가 같다면 y좌표가 작은 벡터일수록
-    
+   
     // 벡터끼리의 덧셈 뺄셈
     vector2 operator + (const vector2& rhs) const {
         return vector2(x+rhs.x, y+rhs.y);
@@ -26,27 +26,27 @@ struct vector2 {
     vector2 operator - (const vector2& rhs) const {
         return vector2(x-rhs.x, y-rhs.y);
     }
-    
+   
     // 실수배
     vector2 operator * (double rhs) const {
         return vector2(x*rhs, y*rhs);
     }
-    
+   
     // 벡터의 길이 반환
     double norm() const {
         return hypot(x, y); // hypot : 직각삼각형 빗변 길이
     }
-    
+   
     // 방향이 같은 단위벡터를 반환
     vector2 normalize() const {
         return vector2(x/norm(), y/norm());
     }
-    
+   
     // x축 양의 방향 기준 반시계 방향 각도
     double polar() const {
         return fmod(atan2(y, x) + 2*PI, 2*PI);
     }
-    
+   
     // 내적 / 외적
     double dot(const vector2& rhs) const {
         return x*rhs.x + y*rhs.y;
@@ -54,11 +54,34 @@ struct vector2 {
     vector2 cross(const vector2& rhs) const {
         return vector2(x*rhs.y, -rhs.x*y);
     }
-    
+   
     // 이 벡터를 rhs에 사영
     vector2 project(const vector2& rhs) const {
         vector2 r = rhs.normalize();
         return r*r.dot(*this);
     }
-    
+   
 };
+
+double between_angle(vector2 a, vector2 b) {
+   
+    double theta = acos(a.dot(b)/(a.norm()*b.norm()));
+   
+    return theta;
+}
+
+bool isRightAngle(vector2 a, vector2 b) {
+   
+    bool yes = (a.dot(b)==0 ? 1 : 0);
+   
+    return yes;
+}
+
+
+int main() {
+    vector2 a = vector2(0,1);
+    vector2 b = vector2(1,0);
+    cout << between_angle(a, b); // 3.14
+    cout << isRightAngle(a, b); // true
+   
+}
