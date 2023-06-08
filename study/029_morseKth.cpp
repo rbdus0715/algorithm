09fd77a5115ffa34c7b542771422070baba7e271
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-06-08 18:20:34 +0900

    Update 029_morseKth.cpp

--- study/029_morseKth.cpp
+++ study/029_morseKth.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 // 모스부호 k번째 계산하기
 
 
@@ -28,13 +30,101 @@ void generate2(int n, int m, string s) {
     if(m>0) generate2(n, m-1, s+'o');
 }
 
+/*
 int main() {
     cin >> skip;
     skip--;
     string a = "";
     generate2(2, 2, a);
 }
+*/
 
 
 
-//
+// 위의 코드에서 조금만 개선을 해보자
+// skip의 수가 남은 m,n개의 점로 만들어질 수 있는 부호 조합이 같거나 크다면 그걸 굳이 세야할까?
+// m+nCm 만큼을 건너뛸 수 있는 것이다
+
+#include <iostream>
+using namespace std;
+
+int skip;
+// k의 최대값 + 100을 상한선으로 잡아 오버플로우가 일어나지 않도록 한다. >> 나중에 이용할 때도 주의할점
+const int M = 1000000000+100;
+int bino[201][201];
+
+void cal_bino() {
+    for(int i=0; i<=200; i++) {
+        bino[i][0] = bino[i][i] = 1;
+        for(int j=0; j<=200; j++) {
+            bino[i][j] = min(M, bino[i-1][j-1] + bino[i-1][j]);
+        }
+    }    
+}
+
+
+void generate2(int n, int m, string s) {
+    if(skip<0) return;
+    if(m==0&&n==0) {
+        if(skip==0) cout << s << '\n';
+        skip--;
+        return;
+    }
+    if(bino[n][m]<=skip) {
+        skip -= bino[n][m];
+        return;
+    }
+    
+    if(n>0) generate2(n-1, m, s+'-');
+    if(m>0) generate2(n, m-1, s+'o');
+}
+
+int main() {
+    cin >> skip;
+    skip--;
+    string a = "";
+    cal_bino();
+    generate2(2, 2, a);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
