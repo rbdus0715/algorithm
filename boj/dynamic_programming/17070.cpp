1d81cb8692c159a7cf1111a86f2ace6965842763
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-06-10 08:48:04 +0900

    Update 17070.cpp

--- boj/dynamic_programming/17070.cpp
+++ boj/dynamic_programming/17070.cpp
@@ -1,27 +1,58 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int N, map[17][17], dp[17][17];
-const int dx[3] = {1, 1, 0};
-const int dy[3] = {0, 1, 1};
+int N, map[17][17], dp[3][17][17];
+// 각 파이프 방향별 움직임
+vector<int> dx[3] = {
+    {1, 1},
+    {0, 1},
+    {1, 0, 1}
+};
+vector<int> dy[3] = {
+    {0, 1},
+    {1, 1},
+    {0, 1, 1}
+};
+// 움직인 후 파이프 방향
+vector<int> after[3] = {
+    {0, 2},
+    {1, 2},
+    {0, 1, 2}
+};
 
-// 1:가로, 2:세로, 3:대각선
-bool canMove(int Type, iny y, int x) {
-    if(Type==1 && x+1<=N) return true;
-    if(Type==2 && y+1<=N) return true;
-    if(Type==3 && (x+1<=N||y+1<=N)) return true;
-    return false;
+bool canMove(int af, int m, int y, int x) {
+
+    if(af!=2 && m!=1) return true;
+    
+    bool ans = true;
+    for(int i=0; i<3; i++) {
+        int ny = y + dy[2][i];
+        int nx = x + dx[2][i];
+        if(map[ny][nx]==1) ans=false;
+    }
+    return ans;
 }
 
-int d(int y, int x) {
-    if(y==N && x==N) return 1;
+int d(int y, int x, int type) {
+    if(y==N && x==N) {
+        return 1;
+    }
     
-    int& ret = dp[y][x];
+    int& ret = dp[type][y][x];
     if(ret != -1) return ret;
     
-    for(int i=1; i<=3; i++) {
-        if(canMove(i, y, x)) // 여기까지 함
+    ret=0;
+    
+    for(int i=0; i<dx[type].size(); i++) {
+        int ny = y + dy[type][i];
+        int nx = x + dx[type][i];
+        if(ny<=N && nx<=N && canMove(after[type][i], map[ny][nx], y, x)) {
+            ret += d(ny, nx, after[type][i]);
+        }
     }
+    
+    return ret;
 }
 
 int main() {
@@ -29,8 +60,8 @@ int main() {
     for(int i=1; i<=N; i++) {
         for(int j=1; j<=N; j++){ 
             cin >> map[i][j];
-            dp[i][j] = -1;
+            dp[0][i][j] = -1; dp[1][i][j] = -1; dp[2][i][j] = -1;
         }
     }
-    cout << d(1, 2);
+    cout << d(1, 2, 0);
 }
