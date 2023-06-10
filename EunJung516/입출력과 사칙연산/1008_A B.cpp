7bbf81344b6c52a846c2ee443e2e8d7c47770a92
EunJung516 <65918936+EunJung516@users.noreply.github.com>
2023-06-11 00:31:05 +0900

    입출력과 사칙연산_eunjung

---
+++ EunJung516/입출력과 사칙연산/1008_A B.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    double a, b;
+    cin >> a >> b;
+    cout.precision(15);
+    cout << a / b;
+}
---
+++ EunJung516/입출력과 사칙연산/10430_나머지.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    int A, B, C;
+    cin >> A >> B >> C;
+    cout << (A + B) % C << '\n'
+         << ((A % C) + (B % C)) % C << '\n'
+         << (A * B) % C << '\n'
+         << ((A % C) * (B % C)) % C;
+}
